92b74a393173d4fc623d1f39bc3263ec9b141351
Methan830 <ethanmangaroo7@gmail.com>
2024-09-09 03:55:48 +0000

    commit on "main"

---
+++ practice.c
@@ -0,0 +1,20 @@
+// Ethan Mangaroo
+// 5629504
+
+# include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+double calculateDistance();
+double calculatePerimeter();
+double calculateArea();
+double calculateWidth();
+double calculateHeight();
+
+double calculateDistance() {
+    double x1, y1, x2, y2;
+    printf("Point #1 entered: ");
+    scanf("%lf, %lf", &x1, &y1);
+    printf("Point #2 entered: ");
+    scanf("%lf, %lf", &x2, &y2);
+    
+}    
